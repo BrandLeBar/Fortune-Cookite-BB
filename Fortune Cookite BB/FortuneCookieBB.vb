8281818504ec92dc7133b0294eeda8d01b99d7b7
Barkly-Brandon <bubba05barrera@gmail.com>
2025-02-09 15:43:18 -0700

    Add project files.

---
+++ Fortune Cookite BB/FortuneCookieBB.vb
@@ -0,0 +1,52 @@
+﻿Option Compare Text
+Module FortuneCookieBB
+
+    Sub Main()
+        Dim userInput As String
+
+        Console.WriteLine("Press ""q"" to quit at anytime.")
+        Console.WriteLine()
+
+        While userInput <> "Q"
+
+            Try
+
+                Console.WriteLine("This your fortune...")
+
+                Select Case RNGesus(1, 3)
+                    Case 1
+                        Console.WriteLine("If at first your voltages don't cancel out try, try again.")
+                        Console.WriteLine()
+                    Case 2
+                        Console.WriteLine("You will find a low test score in your future.")
+                        Console.WriteLine()
+                    Case 3
+                        Console.WriteLine("You will ask for another fortune, very soon.")
+                        Console.WriteLine()
+                End Select
+
+            Catch ex As Exception
+
+            End Try
+
+            Console.WriteLine("Continue? Y/N")
+            userInput = Console.ReadLine
+            Console.WriteLine()
+
+            If userInput = "N" Then
+                Console.WriteLine("Bye Bye")
+                Console.WriteLine()
+                userInput = "Q"
+            ElseIf userInput = "Y" Then
+                Console.WriteLine("You are very fortunate, son.")
+                Console.WriteLine()
+            End If
+        End While
+    End Sub
+
+    Function RNGesus(Min As Integer, Max As Integer) As Integer
+        Randomize()
+        Return CInt((Max - Min) * Rnd() + Min)
+    End Function
+
+End Module
---
+++ Fortune Cookite BB/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Fortune Cookite BB")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Fortune Cookite BB")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("a5891a2c-abb7-4e25-8304-c33d61889404")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
